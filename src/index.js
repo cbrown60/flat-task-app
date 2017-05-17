769fbaaa3c838763e30352f17d3a8842834e7d33
Chris <cbrown605@googlemail.com>
2017-05-17 15:40:57 +0100

    set up new files for index, app,signIn and signUp

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+
+ReactDOM.render(
+  <div>App</div>, document.getElementById("root")
+  ) 
+
+
+
