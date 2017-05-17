5c950eb986bfe0e822fa5045557ff502d2cc0264
Chris <cbrown605@googlemail.com>
2017-05-17 18:22:13 +0100

    firebase file and Sign Up files added, there is a sogn in box which logs the users details and saves to firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase'
+
+
+  var config = {
+    apiKey: "AIzaSyDINrobcZeupss7rHlC47Rq6JfniiR-Qeo",
+    authDomain: "javascriptproject-e2f1f.firebaseapp.com",
+    databaseURL: "https://javascriptproject-e2f1f.firebaseio.com",
+    projectId: "javascriptproject-e2f1f",
+    storageBucket: "javascriptproject-e2f1f.appspot.com",
+    messagingSenderId: "50412313625"
+  };
+
+export const firebaseApp = firebase.initializeApp(config)
+
+
